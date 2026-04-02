2f1d188a716b0ce2904988b8388659b4df91c7aa
An Hyeongsu <anhyeongsu@Anui-MacBookAir.local>
2026-04-02 23:43:32 +0900

    first commit

---
+++ src/main/java/site/gpglab/ossrefapi/OssrefApiApplication.java
@@ -0,0 +1,13 @@
+package site.gpglab.ossrefapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssrefApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OssrefApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/gpglab/ossrefapi/OssrefApiApplicationTests.java
@@ -0,0 +1,13 @@
+package site.gpglab.ossrefapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OssrefApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
